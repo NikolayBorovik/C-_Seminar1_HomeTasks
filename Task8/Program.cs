9b6ba7d452d3b959a423e62fef289942ff79adbb
Nikolay Borovik <nik.Borovik@bk.ru>
2023-01-27 14:56:34 +1000

    Task8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.Write("Enter your number: ");
+string strNumber = Console.ReadLine();
+int numberN = Convert.ToInt32(strNumber);
+
+if(numberN < 1)
+{
+    System.Console.WriteLine("You have entered a wrong number");
+}
+
+int number2 = 1;
+
+while(numberN >= number2)
+{
+    System.Console.Write(number2);
+    System.Console.Write(" ");
+    number2 = number2 + 1;
+}
