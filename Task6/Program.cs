a73c4c02c85a9ff64fe07ab84de2be8b2bb20acb
Nikolay Borovik <nik.Borovik@bk.ru>
2023-01-27 14:43:17 +1000

    Task6 done

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным 
+// (делится ли оно на два без остатка).
+
+System.Console.Write("Enter your number: ");
+string strNumber = Console.ReadLine();
+int number = Convert.ToInt32(strNumber);
+if(number % 2 == 0)
+{
+    System.Console.WriteLine("Your number is EVEN");
+}
+else
+{
+    System.Console.WriteLine("Your number is ODD");
+}
